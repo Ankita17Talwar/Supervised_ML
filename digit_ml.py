815ae2c8d2ac0e770b66127b3c6437e582e682a7
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-28 00:14:25 +0530

    finding accuracy of classification

--- digit_ml.py
+++ digit_ml.py
@@ -9,6 +9,7 @@ digits = datasets.load_digits()
 
 # Print the keys and DESCR of the dataset
 print(digits.keys())
+# dict_keys(['data', 'target', 'frame', 'feature_names', 'target_names', 'images', 'DESCR'])
 
 print(digits['DESCR'])  # digits.DESCR
 
@@ -20,4 +21,19 @@ print(digits.data.shape)
 plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
 
+# Create feature and target arrays
+X = digits.data
+y = digits.target
 
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=7)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
