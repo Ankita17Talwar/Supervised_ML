b349a3b471229d63ab2567250cf0bbbe3d6face8
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-28 00:51:04 +0530

    regression and plot

--- regression_sc1.py
+++ regression_sc1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 boston = pd.read_csv('data/boston.csv')
 
@@ -10,3 +10,30 @@ print(boston.head())
 
 X = boston.drop('MEDV', axis=1).values
 y= boston['MEDV'].values
+
+# Predicting house value from single feature : slice number of rooms column
+X_rooms = X[:,5]
+print(type(X_rooms))
+print(type(y))
+
+y= y.reshape(-1,1)
+X_rooms = X_rooms.reshape(-1,1)
+
+# Plot house value  vs number of rooms
+plt.scatter(X_rooms,y)
+plt.ylabel('Value of house /1000($)')
+plt.xlabel('Number of rooms')
+plt.show()
+
+# Fitting regression model
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+reg = LinearRegression()
+
+reg.fit(X_rooms, y)
+prediction_space = np.linspace(min(X_rooms), max(X_rooms)).reshape(-1,1)
+
+plt.scatter(X_rooms, y, color='blue')
+plt.plot(prediction_space, reg.predict(prediction_space), color='black', linewidth=3)
+plt.show()
