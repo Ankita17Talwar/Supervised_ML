ce5968cf6062e180cb0bc6fb7090ca2012265ca3
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-28 00:36:12 +0530

    create Target and feature array

--- regression_sc1.py
+++ regression_sc1.py
@@ -4,3 +4,9 @@ import pandas as pd
 boston = pd.read_csv('data/boston.csv')
 
 print(boston.head())
+
+# target variable = MEDV - median value
+# Create Featue and Target
+
+X = boston.drop('MEDV', axis=1).values
+y= boston['MEDV'].values
