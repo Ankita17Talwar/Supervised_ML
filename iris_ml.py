f33d6c6a07267ed662d44596c4bc61924d049bc9
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-27 23:26:49 +0530

    knn prediction

--- iris_ml.py
+++ iris_ml.py
@@ -26,3 +26,21 @@ df = pd.DataFrame(X, columns=iris.feature_names)
 
 _ = pd.plotting.scatter_matrix(df, c=Y, figsize=[8, 8], s=150, marker='D')
 plt.show()
+
+# K-NN
+
+from sklearn.neighbors import KNeighborsClassifier
+
+# set number of neighbors
+Knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit classifier to trainning data
+Knn.fit(iris['data'], iris['target'])
+
+# Predict unlabeled data
+X_new = np.array(([5.6, 2.8, 3.9, 1.1],
+                 [5.7, 2.6, 3.8, 1.3],
+                 [4.7, 3.2, 1.3, 0.2]))
+prediction = Knn.predict(X_new)
+
+print(prediction)
