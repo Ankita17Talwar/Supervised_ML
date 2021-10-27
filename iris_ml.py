de936261e2d697b895d8b4c5b8aacf4ae78adf51
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-27 21:35:56 +0530

    Plot iris data

--- iris_ml.py
+++ iris_ml.py
@@ -3,8 +3,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from pandas.plotting import scatter_matrix
+
 plt.style.use('ggplot')
 
-isris = datasets.load_iris()
+iris = datasets.load_iris()
 
-print(type(isris))
+print(type(iris))
+print(iris.keys())
+# dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename'])
+
+# print(iris.data)
+# print(iris.target)
+# print(iris.data.shape)
+print(iris.target_names)
+
+X = iris.data
+Y = iris.target
+df = pd.DataFrame(X, columns=iris.feature_names)
+
+# print(df.head())
+
+_ = pd.plotting.scatter_matrix(df, c=Y, figsize=[8, 8], s=150, marker='D')
+plt.show()
