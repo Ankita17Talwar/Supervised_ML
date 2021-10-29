2baab1fabb0596f8c6a3906d4db916cd4118fcbc
Ankita17Talwar <talwar17ankita@gmail.com>
2021-10-29 22:55:33 +0530

    increase max iter of LogisticRegression to 400. default is 100 LogisticRegression(... solver='lbfgs', max_iter=100 ...)

--- LogisticRegression_Classification.py
+++ LogisticRegression_Classification.py
@@ -20,7 +20,7 @@ y = df['diabetes']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Create the classifier: logreg
-logreg = LogisticRegression()
+logreg = LogisticRegression(max_iter=400)
 
 # Fit the classifier to the training data
 logreg.fit(X_train, y_train)
